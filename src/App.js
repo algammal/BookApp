5685f17bf60b811f2ea10cc8d6d19a78d23c0559
algammal <algammal@eg.ibm.com>
2020-03-09 13:59:50 +0200

    adding api files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+import * as BooksAPI from './Utils/BooksAPI';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class BooksApp extends Component {
+  render(){
   return (
     <div className="App">
       
     </div>
   );
 }
+}
 
-export default App;
+export default BooksApp;
